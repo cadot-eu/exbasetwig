350b5952fe362bee9f81649a703ed0f1d2a1ea22
michael <michael@cadot.info>
2021-11-24 09:47:44 +0000

    add url

--- src/Twig/EnvExtension.php
+++ src/Twig/EnvExtension.php
@@ -12,6 +12,7 @@ class EnvExtension extends AbstractExtension
     {
         return [
             new TwigFunction('getenv', [$this, 'getenv']),
+            new TwigFunction('url', [$this, 'url'])
         ];
     }
 
@@ -25,5 +26,22 @@ class EnvExtension extends AbstractExtension
     public function getenv(string $var)
     {
         return $_ENV[$var];
+    }
+    public function url()
+    {
+        return ("{% set route =
+            path(
+              app.request.attributes.get('_route'),
+              app.request.attributes.get('_route_params')
+            )
+          %}
+          {% set action =
+            path(
+              app.request.attributes.get('_route'),
+              app.request.attributes.get('_route_params')
+            )
+              | split('/')
+              | last
+          %}");
     }
 }
