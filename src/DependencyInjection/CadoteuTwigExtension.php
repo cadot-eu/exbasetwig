776b879af70d362d148ecd18d3e72910d1b0e37b
Michael <m@cadot.eu>
2022-05-04 17:26:48 +0200

    patch

--- src/DependencyInjection/CadoteuTwigExtension.php
+++ src/DependencyInjection/CadoteuTwigExtension.php
@@ -2,10 +2,7 @@
 
 namespace Cadoteu\TwigBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class CadoteuTwigExtension extends Extension
