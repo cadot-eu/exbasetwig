13473b4a3ef0a3bf6779fd79a4f0baa7495c437c
Michael <m@cadot.eu>
2022-05-04 17:28:23 +0200

    patch

--- src/DependencyInjection/CadoteuTwigExtension.php
+++ src/DependencyInjection/CadoteuTwigExtension.php
@@ -2,15 +2,18 @@
 
 namespace Cadoteu\TwigBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class CadoteuTwigExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        // $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        // $loader->load('services.yaml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
         //charge config/packages/test.yaml par lib/Cadotinfo/tools-bundle/DependencyInjection/Configuration.php
         //$config = $this->processConfiguration(new Configuration(), $configs);
     }
